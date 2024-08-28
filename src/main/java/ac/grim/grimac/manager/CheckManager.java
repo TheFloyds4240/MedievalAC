f1988e66beccf71f31c7603b91a44e79f067f996
ManInMyVan <maninmyvan@gmail.com>
2024-08-28 11:53:47 -0600

    Add PacketOrderH

--- src/main/java/ac/grim/grimac/manager/CheckManager.java
+++ src/main/java/ac/grim/grimac/manager/CheckManager.java
@@ -136,6 +136,7 @@ public class CheckManager {
                 .put(PacketOrderE.class, new PacketOrderE(player))
                 .put(PacketOrderF.class, new PacketOrderF(player))
                 .put(PacketOrderG.class, new PacketOrderG(player))
+                .put(PacketOrderH.class, new PacketOrderH(player))
                 .put(GroundSpoof.class, new GroundSpoof(player))
                 .put(OffsetHandler.class, new OffsetHandler(player))
                 .put(SuperDebug.class, new SuperDebug(player))
