0719c5bdc6a499fa132222c3591b7c9858c8db78
ManInMyVan <maninmyvan@gmail.com>
2024-11-09 20:11:22 -0700

    add PacketOrderO and PacketOrderP

--- src/main/java/ac/grim/grimac/manager/CheckManager.java
+++ src/main/java/ac/grim/grimac/manager/CheckManager.java
@@ -103,6 +103,7 @@ public class CheckManager {
                 .put(PacketOrderC.class, new PacketOrderC(player))
                 .put(PacketOrderD.class, new PacketOrderD(player))
                 .put(PacketOrderK.class, new PacketOrderK(player))
+                .put(PacketOrderP.class, new PacketOrderP(player))
                 .put(NoSlowB.class, new NoSlowB(player))
                 .put(SetbackBlocker.class, new SetbackBlocker(player)) // Must be last class otherwise we can't check while blocking packets
                 .build();
@@ -164,6 +165,7 @@ public class CheckManager {
                 .put(PositionPlace.class, new PositionPlace(player))
                 .put(RotationPlace.class, new RotationPlace(player))
                 .put(PacketOrderN.class, new PacketOrderN(player))
+                .put(PacketOrderO.class, new PacketOrderO(player))
                 .put(DuplicateRotPlace.class, new DuplicateRotPlace(player))
                 .put(GhostBlockMitigation.class, new GhostBlockMitigation(player))
                 .build();
