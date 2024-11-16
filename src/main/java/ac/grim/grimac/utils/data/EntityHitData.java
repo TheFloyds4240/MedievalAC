fc1d5ff3ae2487b68f388df45ff2e7336a6ec395
Axionize <154778082+Axionize@users.noreply.github.com>
2024-11-16 01:33:56 -0500

    Add new EntityHitData constructors

--- src/main/java/ac/grim/grimac/utils/data/EntityHitData.java
+++ src/main/java/ac/grim/grimac/utils/data/EntityHitData.java
@@ -8,11 +8,16 @@ import org.bukkit.util.Vector;
 @Getter
 @ToString
 public class EntityHitData extends HitData {
-
     private final PacketEntity entity;
 
     public EntityHitData(PacketEntity packetEntity) {
-        super(new Vector(packetEntity.trackedServerPosition.getPos().x, packetEntity.trackedServerPosition.getPos().y, packetEntity.trackedServerPosition.getPos().z));
+        this(packetEntity, new Vector(packetEntity.trackedServerPosition.getPos().x,
+                packetEntity.trackedServerPosition.getPos().y,
+                packetEntity.trackedServerPosition.getPos().z));
+    }
+
+    public EntityHitData(PacketEntity packetEntity, Vector intersectionPoint) {
+        super(intersectionPoint);  // Use actual intersection point
         this.entity = packetEntity;
     }
 }
