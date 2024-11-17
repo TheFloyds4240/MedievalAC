deb2628f776bd7c708bea264a2aebc70f1acb572
Axionize <154778082+Axionize@users.noreply.github.com>
2024-11-17 07:05:26 -0500

    Optimize if else in updateEntityMetaData()

--- src/main/java/ac/grim/grimac/utils/latency/CompensatedEntities.java
+++ src/main/java/ac/grim/grimac/utils/latency/CompensatedEntities.java
@@ -301,9 +301,7 @@ public class CompensatedEntities {
                     player.compensatedWorld.openShulkerBoxes.add(data);
                 }
             }
-        }
-
-        if (entity instanceof PacketEntityRideable) {
+        } else if (entity instanceof PacketEntityRideable) {
             int offset = 0;
             if (PacketEvents.getAPI().getServerManager().getVersion().isOlderThanOrEquals(ServerVersion.V_1_8_8)) {
                 if (entity.getType() == EntityTypes.PIG) {
@@ -345,9 +343,7 @@ public class CompensatedEntities {
                     ((PacketEntityRideable) entity).hasSaddle = (boolean) striderSaddle.getValue();
                 }
             }
-        }
-
-        if (entity instanceof PacketEntityHorse) {
+        } else if (entity instanceof PacketEntityHorse) {
             if (PacketEvents.getAPI().getServerManager().getVersion().isNewerThanOrEquals(ServerVersion.V_1_9_4)) {
                 int offset = 0;
 
@@ -391,23 +387,7 @@ public class CompensatedEntities {
                     ((PacketEntityHorse) entity).isRearing = (info & 0x40) != 0;
                 }
             }
-        }
-
-        if (PacketEvents.getAPI().getServerManager().getVersion().isNewerThanOrEquals(ServerVersion.V_1_9_4)) {
-            EntityData gravity = WatchableIndexUtil.getIndex(watchableObjects, 5);
-
-            if (gravity != null) {
-                Object gravityObject = gravity.getValue();
-
-                if (gravityObject instanceof Boolean) {
-                    // Vanilla uses hasNoGravity, which is a bad name IMO
-                    // hasGravity > hasNoGravity
-                    entity.hasGravity = !((Boolean) gravityObject);
-                }
-            }
-        }
-
-        if (entity.getType() == EntityTypes.FIREWORK_ROCKET) {
+        } else if (entity.getType() == EntityTypes.FIREWORK_ROCKET) {
             int offset = 0;
             if (PacketEvents.getAPI().getServerManager().getVersion().isOlderThanOrEquals(ServerVersion.V_1_12_2)) {
                 offset = 2;
@@ -430,9 +410,7 @@ public class CompensatedEntities {
                     player.compensatedFireworks.addNewFirework(entityID);
                 }
             }
-        }
-
-        if (entity instanceof PacketEntityHook) {
+        } else if (entity instanceof PacketEntityHook) {
             int index;
             if (PacketEvents.getAPI().getServerManager().getVersion().isOlderThanOrEquals(ServerVersion.V_1_9_4)) {
                 index = 5;
@@ -449,6 +427,20 @@ public class CompensatedEntities {
 
             Integer attachedEntityID = (Integer) hookWatchableObject.getValue();
             ((PacketEntityHook) entity).attached = attachedEntityID - 1; // the server adds 1 to the ID
+        }
+
+        if (PacketEvents.getAPI().getServerManager().getVersion().isNewerThanOrEquals(ServerVersion.V_1_9_4)) {
+            EntityData gravity = WatchableIndexUtil.getIndex(watchableObjects, 5);
+
+            if (gravity != null) {
+                Object gravityObject = gravity.getValue();
+
+                if (gravityObject instanceof Boolean) {
+                    // Vanilla uses hasNoGravity, which is a bad name IMO
+                    // hasGravity > hasNoGravity
+                    entity.hasGravity = !((Boolean) gravityObject);
+                }
+            }
         }
     }
 }
