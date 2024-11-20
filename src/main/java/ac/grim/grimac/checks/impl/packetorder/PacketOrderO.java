12cdd2f3809321f94c8f4878f42c2b8f2b1406c0
ManInMyVan <maninmyvan@gmail.com>
2024-11-20 16:19:51 -0700

    remove PacketOrderO
    depends on too many factors

--- src/main/java/ac/grim/grimac/checks/impl/packetorder/PacketOrderO.java
+++ src/main/java/ac/grim/grimac/checks/impl/packetorder/PacketOrderO.java
@@ -12,9 +12,9 @@ import com.github.retrooper.packetevents.wrapper.play.client.WrapperPlayClientIn
 import com.github.retrooper.packetevents.wrapper.play.client.WrapperPlayClientInteractEntity.InteractAction;
 import com.github.retrooper.packetevents.wrapper.play.client.WrapperPlayClientPlayerFlying;
 
-@CheckData(name = "PacketOrderP", experimental = true)
-public class PacketOrderP extends Check implements PacketCheck {
-    public PacketOrderP(final GrimPlayer player) {
+@CheckData(name = "PacketOrderO", experimental = true)
+public class PacketOrderO extends Check implements PacketCheck {
+    public PacketOrderO(final GrimPlayer player) {
         super(player);
     }
 
