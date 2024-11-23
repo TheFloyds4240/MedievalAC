fb7295b39634ec5fc0374f3a4cb55a37ff6f55e8
Axionize <154778082+Axionize@users.noreply.github.com>
2024-11-22 21:19:28 -0500

    Build with Java 21 toolchain to prevent requiring downloading JDK 18 for contributors

--- build.gradle.kts
+++ build.gradle.kts
@@ -164,6 +164,10 @@ java {
     }
     sourceCompatibility = JavaVersion.VERSION_1_8
     targetCompatibility = JavaVersion.VERSION_1_8
+
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(21))
+    }
 }
 
 tasks.withType<JavaCompile> {
