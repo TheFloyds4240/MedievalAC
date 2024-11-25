c5ceb6df7371d790bf75a1c6a4712c11902eca14
Axionize <154778082+Axionize@users.noreply.github.com>
2024-11-24 20:25:22 -0500

    Cleanup AirLiquidPlace

--- src/main/java/ac/grim/grimac/checks/impl/scaffolding/AirLiquidPlace.java
+++ src/main/java/ac/grim/grimac/checks/impl/scaffolding/AirLiquidPlace.java
@@ -5,7 +5,6 @@ import ac.grim.grimac.api.config.ConfigManager;
 import ac.grim.grimac.checks.CheckData;
 import ac.grim.grimac.checks.type.BlockPlaceCheck;
 import ac.grim.grimac.player.GrimPlayer;
-import ac.grim.grimac.utils.anticheat.LogUtil;
 import ac.grim.grimac.utils.anticheat.update.BlockPlace;
 import ac.grim.grimac.utils.change.BlockModification;
 import ac.grim.grimac.utils.nmsutil.Materials;
@@ -19,26 +18,26 @@ import java.util.Iterator;
 @CheckData(name = "AirLiquidPlace")
 public class AirLiquidPlace extends BlockPlaceCheck {
 
-    private int lastTick;
-    private boolean didLastFlag;
-    private Vector3i lastPlaceLoc;
-    private StateType lastBlockType;
-
     public AirLiquidPlace(GrimPlayer player) {
         super(player);
     }
 
-    /* On 1.9-1.21.1 clients we see:
+    /* This check has been plagued by falses for ages, and I've finally figured it out.
+    When breaking and placing on the same tick in the same tick, I believe the vanilla client always sends DIGGING ACTION packets first
+    This check's falses all seem to stem from processing DiggingAction.START_DIGGING before PacketType.Play.Client.PLAYER_BLOCK_PLACEMENT in the same tick
+    Since we process the break first, when we go to process the place it looks like the player placed against air in the async world
+
+    We will often see:
         Async world updated: short_grass -> air at X: -32, Y: 69, Z: -240, tick 0, cause/source: DiggingAction.START_DIGGING
         AirLiquidPlace Check: Block state at X: -32, Y: 69, Z: -240 is air (valid: false), tick +0, cause/source: PacketType.Play.Client.PLAYER_BLOCK_PLACEMENT <---- previously falsed here
         Async world updated: air -> short_grass at X: -32, Y: 69, Z: -240, tick +3-4, cause: realtime task in applyBlockChanges(List<Vector3i> toApplyBlocks) source: PacketType.Play.Client.PONG
         Async world updated: short_grass -> air at X: -32, Y: 69, Z: -240, tick +0-1, cause: handleNettySyncTransaction(LatencyUtils.java:56) source: PacketType.Play.Client.PONG
-     */
 
-    // This check has been plagued by falses for ages and I"ve finally figured it out.
-    // When breaking and placing on the same tick in the same tick, I believe the vanilla client always sends DIGGING ACTION packets first
-    // We could make a new BadPackets check based on this information, I wonder what we'd catch?, but I'm not 100% certain this is the cause,
-    // This check's falses all seem to stem from processing DiggingAction.START_DIGGING before PacketType.Play.Client.PLAYER_BLOCK_PLACEMENT in the same tick
+        Which previously would've caused a false.
+        To solve this we store recently changed blocks caused by DiggingAction.START_DIGGING (instant breaking) and check against the old block.
+        Lots of other checks have similar issues, and with the new player.blockHistory we can patch those.
+    */
+
     @Override
     public void onBlockPlace(final BlockPlace place) {
         if (player.gamemode == GameMode.CREATIVE) return;
@@ -51,20 +50,6 @@ public class AirLiquidPlace extends BlockPlaceCheck {
                 && blockPos.equals(blockModification.getLocation())
                 && blockModification.getCause() == BlockModification.Cause.START_DIGGING);
 
-        // being debug
-        boolean isValid = !(placeAgainst.isAir() || Materials.isNoPlaceLiquid(placeAgainst));
-
-        // Format the log message
-        String logMessage = String.format(
-                "AirLiquidPlace Check: Block state at %s is %s (valid: %b), tick %d",
-                blockPos,
-                placeAgainst,
-                isValid,
-                currentTick
-        );
-
-        LogUtil.info(logMessage);
-        // end debug
 
         // Check if old block from instant breaking in same tick as the current placement was valid
         // There should only be one block here for legit clients
