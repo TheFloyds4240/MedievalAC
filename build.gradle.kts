1bb0bf7e76f1a8220492d89b28cc0a487c299139
Axionize <154778082+Axionize@users.noreply.github.com>
2024-11-30 00:25:55 -0500

    Java 8 support for main sources -> Java 17

--- build.gradle.kts
+++ build.gradle.kts
@@ -169,8 +169,8 @@ java {
             runtimeClasspath += main.get().output
         }
     }
-    sourceCompatibility = JavaVersion.VERSION_1_8
-    targetCompatibility = JavaVersion.VERSION_1_8
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
 
     toolchain {
         languageVersion.set(JavaLanguageVersion.of(21))
